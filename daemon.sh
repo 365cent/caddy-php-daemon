0113a8dbf781bfa764f53892bc46023c41b2ffaa
365cent <74791570+365cent@users.noreply.github.com>
2023-06-23 20:10:48 -0400

    Update and rename daemon.sh to crontab

--- daemon.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pgrep caddy &> /dev/null
-if [ $? -ne 0 ];then
-        systemctl restart caddy   
-fi
-pgrep php-fpm &> /dev/null
-if [ $? -ne 0 ];then
-        systemctl restart php8.0-fpm   
-fi
